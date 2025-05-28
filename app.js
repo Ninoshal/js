45b4b596473afa37f7c0d43cc4f27311090639cd
Default <ninoshalamberidze@gmail.com>
2025-05-28 11:53:02 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,92 @@
+// let num1 = Math.round(Math.random() * 101) + 100; 
+// let num2 = Math.round(Math.random() * 101) + 300; 
+
+// for (let i = num1; i <= num2; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+
+// let input = prompt("შეიყვანეთ რიცხვი 50 დან 100 მდე");
+// let num = Number(input);
+
+// if (num >= 50 && num <= 100) {
+//   console.log({num});
+  
+//   for (let i = 3; i <= num; i += 3) {
+//     console.log(i);
+//   }
+// } else {
+//   console.log("გთხოვთ, შეიყვანოთ რიცხვი 50 დან 100 მდე.");
+// }
+
+
+
+// let randomNum = Math.round(Math.random() * 101) + 100; 
+// let input = prompt("შეიყვანეთ რიცხვი 300 დან 400 მდე");
+// let userNum = Number(input);
+
+// if (userNum >= 300 && userNum <= 400) {
+//   let start = randomNum < userNum ? randomNum : userNum;
+//   let end = randomNum > userNum ? randomNum : userNum;
+
+//   for (let i = start; i <= end; i++) {
+//     if (i % 3 === 0) {
+//       console.log(i);
+//     }
+//   }
+// } 
+// else {
+//   console.log("შეიყვანეთ რიცხვი 300 დან 400 მდე.");
+// }
+
+
+
+// let strings = ["30", "50", "101", "88"];
+
+// for (let i = 0; i < strings.length; i++) {
+//   console.log(strings[i][0]);
+// }
+
+
+
+// let numbers = [28, 17, 43, 20, 33, 40, 48,47,55, 62, 71, 84, 99];
+
+// for (let n of numbers) {
+//   if (n % 2 === 0) console.log(n + " ლუწია");
+//   else console.log(n + " კენტია");
+// }
+
+
+
+
+// let numbers = [28, 17, 43, 20, 33, 40, 48, 47, 55, 62, 71, 84, 99];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (i % 2 !== 0 && numbers[i] % 2 === 0) {
+//     console.log(numbers[i]);
+//   }
+// }
+
+
+
+// for (let i = -100; i <= 100; i++) {
+//   if (i % 6 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = -100; i <= 100; i++) {
+//   if (i % 4 ===1) {
+//     console.log(i);
+//   }
+// }
+
+
+// let words = ["ძაღლი", "კატა", "ვაშლი", "ბანანი", "მსხალი", "კიტრი"];
+
+// for (let word of words) {
+//   console.log(word);
+// }
